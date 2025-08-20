173d63c67a8dbefbf510c6cf52284bbe3ee61e66
Arun20019048 <pragasarun2@gmail.com>
2025-08-20 23:59:42 +0530

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("Enter first Number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Addition is" + (num1 + num2));
+            Console.WriteLine("Substraction is" + (num1 - num2));
+            Console.WriteLine("Multiplication is " + num1 * num2);
+            Console.WriteLine("divition is " + num1 / num2);
+            Console.WriteLine("Modulus is " + num1 % num2); 
+
+
+
+        }
+    }
+}
